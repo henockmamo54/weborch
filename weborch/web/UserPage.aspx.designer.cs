6b6ce25971cc6d0c5f21bbf8cd1f8de6c89030cc
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2018-05-31 01:27:54 +0900

    editable user page is added

--- weborch/web/UserPage.aspx.designer.cs
+++ weborch/web/UserPage.aspx.designer.cs
@@ -101,5 +101,23 @@ namespace web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView usergridview;
+        
+        /// <summary>
+        /// ListView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView ListView1;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
