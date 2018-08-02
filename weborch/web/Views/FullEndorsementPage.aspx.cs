ab2d165622c9381540429119908c8c0e68623428
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2018-08-03 01:19:50 +0900

    endorser reply page added

---
+++ weborch/web/Views/FullEndorsementPage.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataAccess;
+
+namespace web.Views
+{
+    public partial class FullEndorsementPage : System.Web.UI.Page
+    {
+        Artist artist;
+        Endorser endorser;
+        Artist_Endorser ae;
+        OrchestraDBEntities entity = new OrchestraDBEntities();
+        int userID = -1;
+        int endID = -1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                var mylist = entity.Artist_Endorser.ToList();
+
+                int.TryParse(Request.QueryString["UserID"], out userID);
+                int.TryParse(Request.QueryString["EndID"], out endID);
+
+                if (userID != null && userID > 0) artist = entity.Artists.Where(x => x.ID == userID).FirstOrDefault();
+                if (endID != null && endID > 0) endorser = entity.Endorsers.Where(x => x.ID == endID).FirstOrDefault();
+
+                if (artist != null && endorser != null)
+                {
+                    ArtistName.Text = artist.FirstName;
+                    EndorserName.Text = endorser.Name;
+                    ae = entity.Artist_Endorser.Where(x => x.ArtistID == userID && x.EndorserID == endID).FirstOrDefault();
+                    if (ae != null) endorserComment.Text = ae.Comment;
+                    Session["ae"] = ae;
+                }
+            }
+        }
+
+        public void saveComment(object sender, EventArgs e) {
+
+            if (Session["ae"] != null) {
+                ae = (Artist_Endorser)Session["ae"];
+                ae.Comment = endorserComment.Text;
+                entity.SaveChanges();
+
+                Response.Redirect("~/Views/HomeView");
+            }
+
+        }
+    }
+}
--- weborch/web/Views/HomeView.aspx.cs
+++ weborch/web/Views/HomeView.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +14,31 @@ namespace web
         protected void Page_Load(object sender, EventArgs e)
         {
             
+            //var fromAddress = new MailAddress("iijbiijb14@gmail.com");
+            //var fromPassword = "iijb@654321";
+            //var toAddress = new MailAddress("henockmamo54@gmail.com");
+
+            //string subject = "subject";
+            //string body = "body";
+
+            //System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient
+            //{
+            //    Host = "smtp.gmail.com",
+            //    Port = 587,
+            //    EnableSsl = true,
+            //    DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network,
+            //    UseDefaultCredentials = false,
+            //    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+
+            //};
+
+            //using (var message = new MailMessage(fromAddress, toAddress)
+            //{
+            //    Subject = subject,
+            //    Body = body
+            //})
+            //smtp.Send(message);
+
         }
     }
 }
